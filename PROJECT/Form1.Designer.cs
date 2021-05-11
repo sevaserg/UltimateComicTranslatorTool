2fb2456c10ce89cec7b4a65d085623c24399ac8a
Vsevolod <sevaserg@gmail.com>
2021-05-11 16:31:18 +0300

    Initial commit

---
+++ PROJECT/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace UltimateComicTranslatorTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LP = new System.Windows.Forms.TableLayoutPanel();
+            this.workTLP = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.fwdBtn = new System.Windows.Forms.Button();
+            this.bckBtn = new System.Windows.Forms.Button();
+            this.pageChoice = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.mainMenu.SuspendLayout();
+            this.LP.SuspendLayout();
+            this.workTLP.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenuItem});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(800, 24);
+            this.mainMenu.TabIndex = 0;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // fileMenuItem
+            // 
+            this.fileMenuItem.Name = "fileMenuItem";
+            this.fileMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileMenuItem.Text = "File";
+            // 
+            // LP
+            // 
+            this.LP.ColumnCount = 2;
+            this.LP.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.LP.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.LP.Controls.Add(this.workTLP, 0, 0);
+            this.LP.Controls.Add(this.textBox, 1, 0);
+            this.LP.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LP.Location = new System.Drawing.Point(0, 24);
+            this.LP.Name = "LP";
+            this.LP.RowCount = 1;
+            this.LP.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.LP.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.LP.Size = new System.Drawing.Size(800, 471);
+            this.LP.TabIndex = 1;
+            // 
+            // workTLP
+            // 
+            this.workTLP.ColumnCount = 1;
+            this.workTLP.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.workTLP.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.workTLP.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.workTLP.Controls.Add(this.panel1, 0, 0);
+            this.workTLP.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.workTLP.Location = new System.Drawing.Point(3, 3);
+            this.workTLP.Name = "workTLP";
+            this.workTLP.RowCount = 2;
+            this.workTLP.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90.2381F));
+            this.workTLP.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.761905F));
+            this.workTLP.Size = new System.Drawing.Size(394, 465);
+            this.workTLP.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.63441F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 62.36559F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 109F));
+            this.tableLayoutPanel2.Controls.Add(this.fwdBtn, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.bckBtn, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.pageChoice, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 422);
+            this.tableLayoutPanel2.MinimumSize = new System.Drawing.Size(0, 50);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(388, 50);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // fwdBtn
+            // 
+            this.fwdBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fwdBtn.Location = new System.Drawing.Point(282, 3);
+            this.fwdBtn.Name = "fwdBtn";
+            this.fwdBtn.Size = new System.Drawing.Size(103, 44);
+            this.fwdBtn.TabIndex = 0;
+            this.fwdBtn.Text = "==>";
+            this.fwdBtn.UseVisualStyleBackColor = true;
+            // 
+            // bckBtn
+            // 
+            this.bckBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bckBtn.Location = new System.Drawing.Point(3, 3);
+            this.bckBtn.Name = "bckBtn";
+            this.bckBtn.Size = new System.Drawing.Size(99, 44);
+            this.bckBtn.TabIndex = 1;
+            this.bckBtn.Text = "<==";
+            this.bckBtn.UseVisualStyleBackColor = true;
+            // 
+            // pageChoice
+            // 
+            this.pageChoice.AllowDrop = true;
+            this.pageChoice.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pageChoice.FormattingEnabled = true;
+            this.pageChoice.Location = new System.Drawing.Point(108, 3);
+            this.pageChoice.Name = "pageChoice";
+            this.pageChoice.Size = new System.Drawing.Size(168, 23);
+            this.pageChoice.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.pictureBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(388, 413);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(388, 413);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.Location = new System.Drawing.Point(403, 3);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.Size = new System.Drawing.Size(394, 465);
+            this.textBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 495);
+            this.Controls.Add(this.LP);
+            this.Controls.Add(this.mainMenu);
+            this.MainMenuStrip = this.mainMenu;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Ultimate Comic Translator Tool";
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            this.LP.ResumeLayout(false);
+            this.LP.PerformLayout();
+            this.workTLP.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileMenuItem;
+        private System.Windows.Forms.TableLayoutPanel LP;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.TableLayoutPanel workTLP;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button fwdBtn;
+        private System.Windows.Forms.Button bckBtn;
+        private System.Windows.Forms.ComboBox pageChoice;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ PROJECT/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UltimateComicTranslatorTool
+{
+    public partial class Form1 : Form
+    {
+        Pt[] pts;
+        ToolStripMenuItem[] filesubitems;
+        String currentFolder;
+        String[] pages;
+        int selectedPt = -1;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox.MouseClick += new MouseEventHandler(editCircles);
+            pts = new Pt[1];
+            pts[0] = new Pt(10, 10, "", "Test");
+
+
+            filesubitems = new ToolStripMenuItem[3];
+            for (int i = 0; i < 3; i++)
+                filesubitems[i] = new ToolStripMenuItem();
+            filesubitems[0].Text = "Open";
+            filesubitems[0].Click += new EventHandler(openEvt);
+            filesubitems[1].Text = "Save";
+            filesubitems[1].Click += new EventHandler(saveEvt);
+            filesubitems[2].Text = "Exit";
+            filesubitems[2].Click += new EventHandler(exitEvt);
+            filesubitems[1].Enabled = false;
+            fileMenuItem.DropDownItems.AddRange(filesubitems);
+            LP.ColumnStyles[0].SizeType = SizeType.Percent;
+            LP.ColumnStyles[0].Width = 70;
+            LP.ColumnStyles[1].SizeType = SizeType.Percent;
+            LP.ColumnStyles[1].Width = 30;
+            fwdBtn.Click += new EventHandler(fwdEvt);
+            bckBtn.Click += new EventHandler(bckEvt);
+            textBox.TextChanged += new EventHandler(dataChange);
+
+            pictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+        }
+        
+        private void fwdEvt(object sender, EventArgs e)
+        {
+            int currentIndex = pageChoice.FindStringExact(pageChoice.Text);
+            if (currentIndex < pageChoice.Items.Count - 1 && pageChoice.Items.Count > 0)
+            {
+                currentIndex++;
+                pageChoice.SelectedIndex = currentIndex;
+                redraw();
+            }
+        }
+        private void bckEvt(object sender, EventArgs e)
+        {
+            int currentIndex = pageChoice.FindStringExact(pageChoice.Text);
+            if (currentIndex > 0 && pageChoice.Items.Count > 0)
+            {
+                currentIndex--;
+                pageChoice.SelectedIndex = currentIndex;
+                redraw();
+            }
+        }
+        private void openEvt(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+                folderDialog.ShowDialog();
+                currentFolder = folderDialog.SelectedPath;
+                filesubitems[1].Enabled = true;
+                pages = System.IO.Directory.GetFiles(currentFolder);
+                textBox.Text = "";
+                if (System.IO.File.Exists(currentFolder + "\\savefile.uctt"))
+                {
+                    String s = System.IO.File.ReadAllText(currentFolder + "\\savefile.uctt");
+                    List<Pt> ptsList = new List<Pt>();
+                    if (s.Split("<~~obj_razdel~~>").Length > 0)
+                    {
+                        try
+                        {
+                            foreach (String str in s.Split("<~~obj_razdel~~>"))
+                            {
+                                if (str != "")
+                                {
+                                    Pt newpt = new Pt(int.Parse(str.Split("<~~param_razdel~~>")[0]),
+                                                      int.Parse(str.Split("<~~param_razdel~~>")[1]),
+                                                      str.Split("<~~param_razdel~~>")[2],
+                                                      str.Split("<~~param_razdel~~>")[3]
+                                                      );
+                                    ptsList.Add(newpt);
+                                }
+                            }
+                            pts = ptsList.ToArray();
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Loading error!");
+                        }
+                    }
+                }
+                else
+                {
+                    System.IO.File.Create(currentFolder + "\\savefile.uctt");
+                }
+                pageChoice.Items.Clear();
+                if (pages.Length > 0)
+                {
+                    foreach (string page in pages)
+                        pageChoice.Items.Add(page.Split("\\")[page.Split("\\").Length - 1]);
+                    pageChoice.Enabled = true;
+                    pageChoice.SelectedIndex = 0;
+                    redraw();
+                }
+                else
+                {
+                    pageChoice.Enabled = false;
+                }
+                //textBox.Text += page+ Environment.NewLine;
+            }
+            catch
+            {
+
+            }
+        }
+        private void saveEvt(object sender, EventArgs e)
+        {
+            save();
+
+        }
+        private void save()
+        {
+            String s = "";
+            foreach (Pt pt in pts)
+            {
+                s += pt.X + "<~~param_razdel~~>" + pt.Y + "<~~param_razdel~~>" + pt.file + "<~~param_razdel~~>" + pt.contents + "<~~obj_razdel~~>";
+            }
+            if (System.IO.File.Exists(currentFolder + "\\savefile.uctt"))
+                System.IO.File.Delete(currentFolder + "\\savefile.uctt");
+            System.IO.File.WriteAllText(currentFolder+"\\savefile.uctt", s);
+        }
+        private void exitEvt(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void editCircles(object sender, MouseEventArgs e)
+        {
+            if (pageChoice.Text != "")
+            {
+                if (e.Button == MouseButtons.Right)
+                {
+                    List<Pt> ptsList = new List<Pt>();
+                    bool removePt = false;
+                    foreach (Pt pt in pts)
+                    {
+                        if ((pt.X - 5 > e.X || pt.X + 5 < e.X || pt.Y - 5 > e.Y || pt.Y + 5 < e.Y))
+                            ptsList.Add(pt);
+                        else
+                            removePt = true;
+                    }
+
+                    Pt newpt = new Pt(e.X, e.Y, pageChoice.Text, "");
+                    if (removePt == false)
+                    {
+                        ptsList.Add(newpt);
+                        pts = ptsList.ToArray();
+
+                        selectedPt = pts.Length - 1;
+                    }
+                    else
+                    {
+                        selectedPt = -1;
+                        pts = ptsList.ToArray();
+                    }
+                    rewrite();
+
+                    redraw();
+                }
+                if (e.Button == MouseButtons.Left)
+                {
+                    for (int i = 0; i < pts.Length; i++)
+                    {
+                        Pt pt = pts[i];
+                        if (pt.file == pageChoice.Text && (!(pt.X - 5 > e.X || pt.X + 5 < e.X || pt.Y - 5 > e.Y || pt.Y + 5 < e.Y)))
+                        {
+                            selectedPt = i;
+                            break;
+                        }
+                    }
+                    rewrite();
+                }
+            }
+        }
+        private void rewrite()
+        {
+            if (selectedPt >= 0 && selectedPt < pts.Length)
+            textBox.Text = pts[selectedPt].contents;
+        }
+        private void dataChange(object sender, EventArgs e)
+        {
+            if (selectedPt >= 0)
+            {
+                pts[selectedPt].contents = textBox.Text;
+            }
+        }
+        private void redraw()
+        {
+            if (pageChoice.Enabled)
+            {
+                Image DrawArea;
+                DrawArea = new Bitmap(Image.FromFile(currentFolder + "\\" + pageChoice.Text));
+                
+                Brush mybrush = new SolidBrush(Color.Red);
+                Graphics g = Graphics.FromImage(DrawArea);
+                foreach (Pt pt in pts)
+                {
+                    if (pt.file == pageChoice.Text)
+                    {
+                        g.FillEllipse(mybrush, pt.X-5, pt.Y-5, 10, 10);
+                    }
+                }
+                pictureBox.Size = DrawArea.Size;
+                pictureBox.Image = DrawArea;
+                g.Dispose();
+            }
+        }
+        public class Pt
+        {
+            public int X;
+            public int Y;
+            public String file;
+            public String contents;
+            public Pt(int X_, int Y_, String file_, String contents_)
+            {
+                X = X_;
+                Y = Y_;
+                file = file_;
+                contents = contents_;
+            }
+            public Pt()
+            {
+
+            }
+        }
+    }
+}
